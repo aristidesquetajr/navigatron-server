5a20f453cd969925e83f4025eff96b2391706190
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-06-30 14:14:20 +0100

    initial project setup

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+import app from './app';
+
+const port = Number(process.env.SERVER_PORT);
+
+app.listen(port, () => console.log(`Server running\n-> http://localhost:${port}`));
